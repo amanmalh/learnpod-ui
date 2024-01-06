2b8702b4a77018038f0c504382b8e5acb486c1e9
amanmalh <amandeep.malhotra.894@gmail.com>
2024-01-06 18:16:55 +0530

    code refactoring

--- src/screens/topic/DeleteTopicDialog.jsx
+++ src/screens/topic/DeleteTopicDialog.jsx
@@ -53,26 +53,6 @@ const DeleteTopicDialog = ({ id, isOpen, setIsOpen }) => {
       </ModalContent>
     </Modal>
   );
-  return (
-    <dialog id="confirm-delete-topic" className="modal">
-      <div className="modal-box">
-        <h3 className="font-bold text-lg">Delete Topic</h3>
-        <p className="py-4">
-          Warning! This will permanently remove this topic. If you still wish to
-          proceed, please click on the Delete button.
-        </p>
-
-        <div className="modal-action">
-          <button onClick={handleDeleteConfirmClick} className="btn">
-            {deleteMutation.isLoading && (
-              <span className="loading loading-spinner text-primary"></span>
-            )}
-            Delete
-          </button>
-        </div>
-      </div>
-    </dialog>
-  );
 };
 
 export default DeleteTopicDialog;
