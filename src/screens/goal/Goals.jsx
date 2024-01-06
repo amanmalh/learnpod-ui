153555a034a338803712864258eb74bf5e2df4f8
amanmalh <amandeep.malhotra.894@gmail.com>
2024-01-06 11:33:55 +0530

    Migrated goals page to chakra

--- src/screens/goal/Goals.jsx
+++ src/screens/goal/Goals.jsx
@@ -1,19 +1,12 @@
-import React, { useEffect } from "react";
+import React from "react";
 import { useQuery } from "react-query";
+import { Flex, Button, Spacer, Text } from "@chakra-ui/react";
 import { getGoals } from "../../utils/api-utils";
-import { useNavigate } from "react-router-dom";
 import EditGoalDialog from "./EditGoalDialog";
+import GoalItem from "./GoalItem";
 
 const Goals = () => {
-  const navigate = useNavigate();
-
   const goals = useQuery(["goals"], getGoals);
-
-  const openGoal = (goalId) => {
-    return () => {
-      navigate(`/goal/${goalId}`);
-    };
-  };
 
   const handleNewGoalClick = () => {
     document.getElementById("edit-goal-modal").showModal();
@@ -21,26 +14,23 @@ const Goals = () => {
 
   return (
     <>
-      <div className="flex flex-row items-center justify-between">
-        <h1 className="text-2xl">Goals</h1>
-        <button className="btn btn-primary" onClick={handleNewGoalClick}>
+      <Flex>
+        <Text as="h1" fontSize="2xl">
+          Goals
+        </Text>
+        <Spacer />
+        <Button
+          colorScheme="purple"
+          variant="outline"
+          onClick={handleNewGoalClick}
+        >
           New Goal
-        </button>
-      </div>
+        </Button>
+      </Flex>
       {goals.isSuccess && (
         <div className="mt-2 grid md:grid-cols-3 gap-4">
           {goals.data.map((goal) => (
-            <div key={goal.id} className="card bg-base-100 shadow-xl">
-              <div className="card-body">
-                <button
-                  className="btn btn-sm card-title inline-block w-fit"
-                  onClick={openGoal(goal.id)}
-                >
-                  {goal.attributes.title}
-                </button>
-                <p>{goal.attributes.description}</p>
-              </div>
-            </div>
+            <GoalItem key={goal.id} goal={goal} />
           ))}
         </div>
       )}
